9f1216e7b855286434e5d9187f541b1020bdf4d3
germanlizondo <german9lp@gmail.com>
2019-04-13 16:22:12 +0200

    verison 0.1

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//start server
+const server = app.listen(app.get('port'), () => {
+    console.log('App listening on port 3000!');
+});
+
+
+
+//socketIO settings
+const socketIO = require('socket.io');
+const io = socketIO.listen(server);
+
+
+//websockets
+io.on('connection', (socket) => {
+    console.log("new connection")
+
+    socket.on("new message", (data) => {
+        console.log(data);
+    })
+});
